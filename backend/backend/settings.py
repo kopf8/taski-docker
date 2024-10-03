12d30f6ad58fb27f43b898ac60f52dba88d60117
Maria Kirsanova <kopf@list.ru>
2024-10-03 21:13:01 +0300

    Fixed secrets & TG message

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,16 +1,16 @@
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '89.169.167.69',
-                 'yandex-taski.hopto.org']
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 # Application definition
 
